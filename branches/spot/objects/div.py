a6cc0ec60f12d1ddf02b8f3be49e15e5d1b81bb7
weap88 <weap88@4c5cefe2-c847-0410-acef-f5dffbb544d5>
2010-05-10 22:16:15 +0000

    modificado el sistema de pintado de los objetos de tipo div para que solo se pinten si estan dentro del area del padre, actualizado los eventos para que se ajusten a este sistema

--- branches/spot/objects/div.py
+++ branches/spot/objects/div.py
@@ -6,7 +6,7 @@ import re
 class div(pygame.sprite.Sprite, module.Module):
 	def __init__(self, parent=None, content="div", color_content="[0,0,0,0]", width="75", height="23", vertical_alignment="", \
 					horizontal_alignment="", margin="[15,15,15,15]", background="[10,128,10,0]", \
-					border_color = "[255,128,128,0]", border_width = "0"):
+					border_color="[255,128,128,0]", border_width="0", overflow="hidden" ):
 		module.Module.__init__(self)
 		pygame.sprite.Sprite.__init__(self)
 		
@@ -20,13 +20,15 @@ class div(pygame.sprite.Sprite, module.Module):
 		self.vertical_alignment = vertical_alignment
 		self.horizontal_alignment = horizontal_alignment
 		self.margin = map(int, re.match("\[(\d+),(\d+),(\d+),(\d+)\]",margin).groups())
-		self.rect = pygame.Rect(0, 0, self.width , self.height)
+		self.container = self.rect = pygame.Rect(0, 0, self.width , self.height)
 		
 		self.update_position()
 		
 		self.background = map(int, re.match("\[(\d+),(\d+),(\d+),(\d+)\]", background).groups())
 		self.border_color = map(int, re.match("\[(\d+),(\d+),(\d+),(\d+)\]", border_color).groups())
 		self.border_width = int(border_width)
+		#self.border_color = [0,0,0,255]
+		#self.border_width = 2
 		
 		##self.bind("videoresize", self.update_position)
 
@@ -55,30 +57,31 @@ class div(pygame.sprite.Sprite, module.Module):
 	def update_position(self,*args):
 		if not self.parent is None:
 			if self.vertical_alignment.lower() == "bottom":
-				top = self.parent.rect.h-self.parent.border_width*2 - (self.rect.h + self.margin[3])
+				top = self.parent.container.h - (self.rect.h + self.margin[3])
 				height = self.height
 			elif self.vertical_alignment.lower() == "top":
 				top = self.margin[1]
 				height = self.height
 			else:
 				top = self.margin[1]
-				height = self.parent.rect.h-self.parent.border_width*2 - (self.margin[1] + self.margin[3])
+				height = self.parent.container.h - (self.margin[1] + self.margin[3])
 					
 			if self.horizontal_alignment.lower() == "right":
-				left = self.parent.rect.w-self.parent.border_width*2 - (self.rect.w + self.margin[2])
+				left = self.parent.container.w - (self.rect.w + self.margin[2])
 				width = self.width
 			elif self.horizontal_alignment.lower() == "left":
 				left = self.margin[0]
 				width = self.width
 			else:
 				left = self.margin[0]
-				width = self.parent.rect.w-self.parent.border_width*2 - (self.margin[0] + self.margin[2])
+				width = self.parent.container.w - (self.margin[0] + self.margin[2])
 				
 			height = max(0, height)
 			width = max(0, width)
 			
 			self.rect = pygame.Rect(left, top, width, height)
-			self.rect.move_ip(self.parent.rect.x+self.parent.border_width, self.parent.rect.y+self.parent.border_width)
+			self.container = self.rect.move(self.parent.container.x, self.parent.container.y)
+			self.container = self.container.inflate(self.border_width * -2, self.border_width * -2)
 			self.update_surface()
 			
 			for child in self.get_childs():
@@ -86,7 +89,7 @@ class div(pygame.sprite.Sprite, module.Module):
 			
 		
 	def update(self):
-		self.g.draw(core.core.video.get_screen())
+		self.g.draw(core.core.video.get_screen().subsurface(self.parent.container))
 		for child in self.get_childs():
 			child.update()
 		
