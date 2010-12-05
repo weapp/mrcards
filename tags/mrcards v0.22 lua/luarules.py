a1f8d1b34e1ad627dad190c64907bbd3ce7eac16
weap88@gmail.com <weap88@gmail.com@4c5cefe2-c847-0410-acef-f5dffbb544d5>
2010-12-05 13:22:43 +0000

    cambios en mrcards lua, deck ya tiene getitem para atributos

--- tags/mrcards v0.22 lua/luarules.py
+++ tags/mrcards v0.22 lua/luarules.py
@@ -4,88 +4,74 @@ from lupa import LuaRuntime
 
 def load(filename, pyvars, objs, lua):
     lua.execute("""\
-rules = {}
+rules = {
+    select = function() end,
+    clear_selection = function() end,
+    throw_cards = function(selection) end,
+    throwable_selection = function(selection) return true end,
+    draw_a_card = function() end,
+    deal = function() end,
+    pass_turn = function() end,
+    end_turn = function() end,
+    ending_turn = function() end,
+    is_round_finished = function() return false end,
+    end_of_round = function() end,
+    new_round = function() end,
+    new_turn = function() end,
+    terminable_turn = function() return true end,
+    points = function() return 1 end,
+    init = function() end,
+    deckdraws = {},
+    caption = "Sin Nombre",
+    descruption = "Sin Descripcion",
+    playzone = 1,
+    keys_descriptions="",
+    down_func={},
+    throws = {}
+}
 py = {}
-function rules:select() end
-function rules:clear_selection() end
-function rules:throw_cards(selection) end
-function rules:throwable_selection(selection) return true end
-function rules:draw_a_card() end
-function rules:deal() end
-function rules:pass_turn() end
-function rules:end_turn() end
-function rules:ending_turn() end
-function rules:is_round_finished() return false end
-function rules:end_of_round() end
-function rules:new_round() end
-function rules:new_turn() end
-function rules:terminable_turn() return true end
-function rules:points() return 1 end
-function rules:init() end
-rules.deckdraws = {}
-rules.caption = "Sin Nombre"
-rules.descruption = "Sin Descripcion"
-rules.playzone = 1
-rules.keys_descriptions=""
-rules.down_func={}
-rules.throws = {}
 """)
-
-    for key, value in objs.items():
-       lua.globals()['py'][key] = value
-       
+    lua.globals()["list"] = lambda elem: list(elem.values())
+    lua.globals()["keys"] = lambda elem: dict((getattr(pygame, key), strfunc) \
+                            for key,strfunc in elem.items() )
+    lua.globals()["list2table"] = lambda elem: lua.table(*elem)
     for key, value in pyvars.items():
        lua.globals()[key] = value
-
-    
+    for key, value in objs.items():
+       lua.globals()['py'][key] = value
+    lua.globals()["list"] = lambda elem: list(elem.values())
     #print [a for a in lua.globals().items() if a[0] == "python"]
     f = file('rules/' + filename + '.lua')
     code = f.read()
     f.close()
     lua.execute(code)
-    
-    class rules_prox:
-        def __init__(self, rules):
-            self.rules = rules
+    lua.execute("""\
+irules = {
+    caption = rules.caption,
+    description = rules.description,
+    playzone = rules.playzone,
+    deckdraws = rules.deckdraws,
+    keys_descriptions = rules.keys_descriptions,
+    down_func = rules.down_func,
+    throwable_selection = function(selection) return rules:throwable_selection(list2table(selection)) end,
+    throw_cards = function(selection) return rules:throw_cards(list2table(selection)) end,
+    new_round = function() return rules:new_round() end,
+    select = function() return rules:select() end,
+    clear_selection = function() return rules:clear_selection() end,
+    draw_a_card = function() return rules:draw_a_card() end,
+    deal = function() return rules:deal() end,
+    pass_turn = function() return rules:pass_turn() end,
+    end_turn = function() return rules:end_turn() end,
+    ending_turn = function() return rules:ending_turn() end,
+    is_round_finished = function() return rules:is_round_finished() end,
+    end_of_round_roun = function() return rules:end_of_round() end,
+    new_turn = function() return rules:new_turn() end,
+    terminable_turn = function() return rules:terminable_turn() end,
+    points = function(number,suit) return rules:points(number,suit) end,
+    init = function() return rules:init() end
+}    """)
 
-        def __getattr__(self, attr):
-            if attr == "down_func":
-                return dict((getattr(pygame, key), list(strfunc.values()) if \
-                             hasattr(strfunc,'values') else strfunc) \
-                            for key,strfunc in self.rules.down_func.items() )
-            elif attr in ["throwable_selection", "throw_cards"]:
-                f = getattr(self.rules, attr)
-                def colons(selection):
-                    return f(self.rules, lua.table(*selection))
-                return colons
-            elif attr in ["select", "clear_selection", "draw_a_card", "deal", "pass_turn", "end_turn", "ending_turn", "is_round_finished", "end_of_round_roun", "new_turn", "terminable_turn", "points", "init"]:
-                f = getattr(self.rules, attr)
-                def colons(*args, **kws):
-                    return f(self.rules, *args, **kws)
-                return colons
-            else:
-                return getattr(self.rules, attr)
-    
-    rules =  lua.eval("rules")
-    
-    
-    #deck_draws...
-    
-    for k, ideck in rules.deckdraws.items():
-        t = {
-            "name" : ideck.name,
-            "numbers" : list(ideck.numbers.values()),
-            "suits" : list(ideck.suits.values()),
-        }
-        rules.deckdraws[k] = t
-
-    rules.deckdraws = list(rules.deckdraws.values())
-
-    #...deck_draws
-    
-    rules = rules_prox(rules)
-    
-    return rules
+    return lua.eval("irules")
     
 
 lua = LuaRuntime()
